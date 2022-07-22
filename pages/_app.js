86714381698dc8c3681253c6b5b2e984b3b95489
Maria Rita Benate <mariaritabenate@Marias-MacBook-Air.local>
2022-07-21 21:45:13 -0300

    implement header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+   <Head>
+        <title>Batatinha</title>
+        <meta name="description" content="Batatinha batatinha" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps}/>
+  </>
+ 
 }
 
 export default MyApp
