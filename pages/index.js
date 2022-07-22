e70bdf8417da99b57d26e0f41181d747e97ee3ce
Maria Rita Benate <mariaritabenate@Marias-MacBook-Air.local>
2022-07-22 13:28:02 -0300

    test layout style

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="border-1 border-grey-500 max-w-screen-xl mx-auto ml-40 py-16">
         <div className="text-2xl font-light py-4">Ux &amp; Product Designer</div>
         <div className="text-6xl font-medium py-4">hey there, I&apos;m <span className="text-orange-300">Maria</span></div>
-        <div className="text-xl font-light text-neutral-400 py-4">I'm current reading the last chapter of Harry Potter and The Chamber Of Secrets</div>
+        <div className="text-xl font-light text-neutral-400 py-4">I&apos;m current reading the last chapter of Harry Potter and The Chamber Of Secrets</div>
       </div>
       <div className="border-2 border-grey-500 max-w-screen-xl mx-auto py-40">
       </div>
