29791123cc5f19f0d563c70a4f9975c4753fd891
Maria Rita Benate <mariaritabenate@Marias-MacBook-Air.local>
2022-07-22 13:23:55 -0300

    test layout style

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <Link href="/sobre-mim" passHref><a className="text-sm font-medium tracking-wide text-gray-600">Sobre mim</a></Link>
       </div>
       <div className="border-1 border-grey-500 max-w-screen-xl mx-auto ml-40 py-16">
-        <div className="text-2xl font-light py-4">Ux &amp; Product Designer</div>
+        <div className="text-2xl font-light py-4">Ux / Product Designer</div>
         <div className="text-6xl font-medium py-4">hey there, I'm <span className="text-orange-300">Maria</span></div>
         <div className="text-xl font-light text-neutral-400 py-4">I'm current reading the last chapter of Harry Potter and The Chamber Of Secrets</div>
       </div>
